d9640212602c37fd25aa2824fbcf638438955b89
ToniVR <72858333+TogoStyle@users.noreply.github.com>
2022-01-27 12:01:37 +0100

    Lore agregado y musica

--- BatoiBoy1.0/Assets/Scripts/PantallaInicio.cs
+++ BatoiBoy1.0/Assets/Scripts/PantallaInicio.cs
@@ -10,6 +10,8 @@ public class PantallaInicio : MonoBehaviour
     {
         SceneManager.LoadScene("Bosque");
     }
+
+    
     // Start is called before the first frame update
     void Start()
     {
